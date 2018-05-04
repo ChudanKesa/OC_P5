717ec8f19d166a8d70abfd0a3e8f82660bb5d922
Erwan Le Querré <Adm@macbook-de-erwan-le-querre.home>
2018-05-04 08:35:27 +0200

    Main view elements also in place

--- InstaGrid/Sources/Main View/HomeViewController.swift
+++ InstaGrid/Sources/Main View/HomeViewController.swift
@@ -14,6 +14,22 @@ class HomeViewController: UIViewController {
     
     // MARK: - Outlets & actions
     
+    @IBOutlet weak var instagridTitle: UITextView!
+    @IBOutlet weak var instructionText: UITextView!
+    
+    @IBOutlet weak var photoView: PhotosView!
+    
+    @IBOutlet weak var rightButton: UIButton!
+    @IBOutlet weak var centerButton: UIButton!
+    @IBOutlet weak var leftButton: UIButton!
+    
+    
+    @IBAction func rightButtonTouched(_ sender: UIButton) {
+    }
+    @IBAction func centerButtonTouched(_ sender: UIButton) {
+    }
+    @IBAction func leftButtonTouched(_ sender: UIButton) {
+    }
     // MARK: - View life cycle
     
     // MARK: - Notifications
